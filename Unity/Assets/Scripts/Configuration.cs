be674dd44485d09036a0a1c2a3fc715e3646efb4
FinnReimann <103571502+FinnReimann@users.noreply.github.com>
2023-06-04 23:05:23 +0200

    Configuration Script with getter and setter
    config.DemoMode gibt den wert aus, config.DemoMode = false setzt den wert

--- Unity/Assets/Scripts/Configuration.cs
+++ Unity/Assets/Scripts/Configuration.cs
@@ -4,18 +4,89 @@ using UnityEngine.Serialization;
 
 public class Configuration : MonoBehaviour
 {
-    public bool demoMode;
-    
-    public int rayCount;
-    public float minAngle;
-    public float maxAngle;
+    [SerializeField]
+    [Header("Demo Settings")]
+    private bool demoMode;
 
-    public float maxRayLength;
-    public float maxSpeed;
-    public float steeringBehavior;
+// Ray-Einstellungen
+    [SerializeField]
+    [Header("Ray Settings")]
+    private int rayCount;
+    [SerializeField] private float minAngle;
+    [SerializeField] private float maxAngle;
+    [SerializeField] private float maxRayLength;
+    [SerializeField] private float detectionAngle;
 
-    public float detectionAngle;
-    public float accelerationBehavior;
-    
-    public LayerMask raycastMask; // Layer-Maske für den Raycast
+// Geschwindigkeit und Steuerungsverhalten
+    [SerializeField]
+    [Header("Speed and Steering Behavior Settings")]
+    private float maxSpeed;
+    [SerializeField] private float steeringBehavior;
+    [SerializeField] private float accelerationBehavior;
+
+// Erkennungseinstellungen
+    [FormerlySerializedAs("raycastMask")]
+    [SerializeField]
+    [Header("Layermask")]
+    private LayerMask layerMask; 
+
+    public bool DemoMode
+    {
+        get => demoMode;
+        set => demoMode = value;
+    }
+
+    public int RayCount
+    {
+        get => rayCount;
+        set => rayCount = value;
+    }
+
+    public float MinAngle
+    {
+        get => minAngle;
+        set => minAngle = value;
+    }
+
+    public float MaxAngle
+    {
+        get => maxAngle;
+        set => maxAngle = value;
+    }
+
+    public float MaxRayLength
+    {
+        get => maxRayLength;
+        set => maxRayLength = value;
+    }
+
+    public float MaxSpeed
+    {
+        get => maxSpeed;
+        set => maxSpeed = value;
+    }
+
+    public float SteeringBehavior
+    {
+        get => steeringBehavior;
+        set => steeringBehavior = value;
+    }
+
+    public float DetectionAngle
+    {
+        get => detectionAngle;
+        set => detectionAngle = value;
+    }
+
+    public float AccelerationBehavior
+    {
+        get => accelerationBehavior;
+        set => accelerationBehavior = value;
+    }
+
+    public LayerMask LayerMask
+    {
+        get => layerMask;
+        set => layerMask = value;
+    }
 }
--- Unity/Assets/Scripts/Tommy.cs
+++ Unity/Assets/Scripts/Tommy.cs
@@ -1,52 +1,12 @@
+using System;
 using UnityEngine;
 
 public class Tommy : MonoBehaviour
 {
-    private bool[] _raysHits;
     private Configuration _configuration;
-    private Ray _ray;
-    private RaycastHit _hit;
 
     private void Awake()
     {
         _configuration = GetComponent<Configuration>();
-    }
-    
-    private void Start()
-    {
-        _raysHits = new bool[_configuration.rayCount];
-    }
-
-    private void SendRays()
-    {
-        Transform currentTransform = transform;
-        
-        // Create and Send Rays
-        for (int i = 0; i < _configuration.rayCount; i++)
-        {
-            // Create Rays
-             _ray = new Ray(currentTransform.position, currentTransform.forward);
-
-             // Send Rays
-            if (Physics.Raycast(_ray, out _hit, Mathf.Infinity, _configuration.raycastMask))
-            {
-                // Zeichne Raycast
-                Debug.DrawLine(_ray.origin, _hit.point, Color.green);
-                
-                _raysHits[i] = true;
-            }
-            else
-            {
-                // Zeichne Raycast
-                Debug.DrawLine(_ray.origin, _ray.origin + _ray.direction * 10f, Color.black);
-                
-                _raysHits[i] = false;
-            }
-        }
-    }
-
-    public void GetPressure()
-    {
-        
     }
 }
