9a5978f7b2e2da42338ba2088f0fe73748e740d5
chiffred <chiffred@posteo.de>
2023-06-07 18:52:18 +0200

    pushpush obersver interface

--- Unity/Assets/Scripts/Brain.cs
+++ Unity/Assets/Scripts/Brain.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Brain : MonoBehaviour
+public class Brain : MonoBehaviour, Observer
 {
     // Start is called before the first frame update
     void Start()
--- Unity/Assets/Scripts/Observer.cs
+++ Unity/Assets/Scripts/Observer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Observer : MonoBehaviour
+public interface Observer
 {
     // Start is called before the first frame update
     void Start()
